0c8ffc09bc5553e9a101ed3ff23364b3f53890d0
karol <karol@karol-Lenovo-G500s>
2017-12-03 11:32:34 +0100

    dodanie nowej zmiany2

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,12 +3,7 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-System.out.println("nowy");
-System.out.println("nowy");
-System.out.println("nowy4");
-
-
-System.out.println("nowy4");
+System.out.println("Dodanie nowej zmiany");
 	}
 
 }
