742784e004ef6ccbad24b52853639498083c8f3b
karol <karol@karol-Lenovo-G500s>
2017-12-03 11:22:51 +0100

    fff

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,15 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 System.out.println("nowy");
-System.out.println("nowy");
-System.out.println("nowy4");
 
-System.out.println("nowy");
-System.out.println("nowy4");
-System.out.println("nowy");
-System.out.println("nowy4");
-System.out.println("nowy");
-System.out.println("nowy4");
 	}
 
 }
